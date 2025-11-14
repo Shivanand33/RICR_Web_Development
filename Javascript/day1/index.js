2fc670410e6e65c4717e71ae5117786e5e239d3c
Shivanand33 <chaurasiyashivanand01@gmail.com>
2025-11-15 01:04:56 +0530

    Bootstrap, LoginRagistation

---
+++ Javascript/day1/index.js
@@ -0,0 +1,22 @@
+var a = 10;
+
+var a = 20;
+
+let b = 10
+
+console.log(a, b);
+
+let ar = ["ABC", 1235, 45.76, 'a', "true"]
+console.log(arr);
+
+alert("this is alert");
+
+console.warn("this is warning");
+console.error("ye error hai");
+if (confirm("Are you sure")) {
+    console.log()
+}
+
+
+
+

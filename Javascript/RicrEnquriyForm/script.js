45474e6a9eb1f02caecf3d945a691dafeb2b6df5
Shivanand33 <chaurasiyashivanand01@gmail.com>
2025-11-18 20:05:36 +0530

    javascript,RicrEnquriyForm

---
+++ Javascript/RicrEnquriyForm/script.js
@@ -0,0 +1,41 @@
+
+
+function Sumbit() {
+    console.log("Clicked");
+    const Personename = document.getElementById("Personename").value;
+    const Contactnumber = document.getElementById("Contactnumber").value;
+    const email = document.getElementById("email").value;
+    const Qualification = document.getElementById("Qualification").value;
+    const Collagname = document.getElementById("Collagname").value;
+    const year = document.getElementById("year").value;
+    const Branch = document.getElementById("Branch").value;
+    const box = document.getElementById("box").value;
+    const Information = document.getElementById("Information").value;
+    const Excusive = document.getElementById("Excusive").value;
+
+    console.log(Personename);
+    console.log(Contactnumber);
+    console.log(email);
+    console.log(Qualification);
+    console.log(Collagname);
+    console.log(year);
+    console.log(Branch);
+    console.log(box);
+    console.log(Information);
+    console.log(Excusive);
+
+    
+    alert("Sumbit done")
+
+document.getElementById("Personename").value = "";
+document.getElementById("Contactnumber").value = "";
+document.getElementById("email").value;
+document.getElementById("Qualification").value = "";
+document.getElementById("Collagname").value = "";
+document.getElementById("year").value = "";
+document.getElementById("Branch").value = "";
+document.getElementById("box").value;
+document.getElementById("Information").value = "";
+document.getElementById("Excusive").value = "";
+  
+}

7b4a86b5ea4c0ba4be46715ea1048bdad26fddf7
Shivanand33 <chaurasiyashivanand01@gmail.com>
2025-11-22 15:47:31 +0530

    RagistationForminClass

--- Bootstrap/RagistationFormInClass/script.js
+++ Bootstrap/RagistationFormInClass/script.js
@@ -1,23 +1,76 @@
-function sumbit() {
-    // console.log("Clicked sumbit button")
-    
+// function sumbit() {
 
-     
-    document.querySelectorAll("input[name='batch']:checked");  
-    batch.forEach((element) => {
-   selectedTiming.push(element.value);
-    });
 
-// const selsectedBatch = batch.forEach()
- console.log(selectedTiming);
 
- const selcectedBatch = document.querySelector(
-    "input[name='timing']:cheaked"
- ).value;
+//     document.querySelectorAll("input[name='batch']:checked");  
+//     batch.forEach((element) => {
+//    selectedTiming.push(element.value);
+//     });
 
- console.log(selcectedBatch);
+
+//  console.log(selectedTiming);
+
+//  const selcectedBatch = document.querySelector(
+//     "input[name='timing']:cheaked"
+//  ).value;
+
+//  console.log(selcectedBatch);
+// }
+// function clearForm() {
+//     window.location.reload();
+// }
+
+
+function submit() {
+
 }
+function submit() {
+    const fn = document.getElementById("FullName").value;
+    const em = document.getElementById("email").value;
+    const mb = document.getElementById("mobile").value;
+    const dob = document.getElementById("dob").value;
+    const ql = document.getElementById("qualification").value;
+    const sr = document.getElementById("score").value;
+    const cr = document.getElementById("course").value;
+    const nam = document.getElementById("name").value;
+    const mo = document.getElementById("mobile").value;
+    const sp = document.getElementById("special").value;
+    let Preferedbatch = [];
+    document
+        .querySelectorAll("input[name='batch']:checked")
+        .forEach((element) => {
+            Preferedbatch.push(element.value);
+        });
+    const pt = document.querySelector("input[name='timing']:checked").value;
+    const ad = document.getElementById("address").value;
+    const ct = document.getElementById("city").value;
+    const pn = document.getElementById("pin").value;
+
+    let hu = [];
+    document
+        .querySelectorAll("input[name='about']:checked")
+        .forEach((element) => {
+            hu.push(element.value);
+        });
+
+    console.log("Full Name: " + fn);
+    console.log("Email: " + em);
+    console.log("Mobile: " + mb);
+    console.log("Date of Birth: " + dob);
+    console.log("Qualification: " + ql);
+    console.log("Score: " + sr);
+    console.log("Course: " + cr);
+    console.log("Preferred Batch: " + Preferedbatch.join(", "));
+    console.log("Preferred Timing: " + pt);
+    console.log("Address: " + ad);
+    console.log("City: " + ct);
+    console.log("Pin Code: " + pn);
+    console.log("Father Name: " + nam);
+    console.log("Mobile: " + mo);
+    console.log("Special Requirments: " + sp);
+    console.log("How did you hear about us:" + hu.join(","));
+}
+
 function clearForm() {
     window.location.reload();
 }
-

e98a7de4d5d0cc757e05dd09de79cd2409e7eb9b
Shivanand33 <chaurasiyashivanand01@gmail.com>
2026-01-09 16:53:05 +0530

    Update in home page

--- react/Tech-Project/src/Components/Header.jsx
+++ react/Tech-Project/src/Components/Header.jsx
@@ -3,23 +3,62 @@ import React from 'react'
 const Header = () => {
   return (
      <>
-     <nav className='bg-white-400 flex items-center p-2 justify-between w-full  py-2  bottom-1 border-1  border-gray-300'>
-        <div className='text-3xl ms-10 font-bold'>  
-        <span className=' text-yellow-500'>Mon</span>
-        <span className=' text-red-500'>ke</span>
-        <span className=' text-black-300'>y</span>
-         
-        </div>
-        <div>
-         <input type="text" name="search" id="search" placeholder='search product' className='border-1 p-1 rounded-3xl w-3xl' />
-        </div>
+   <nav className="
+  bg-white
+  flex flex-col md:flex-row
+  items-center
+  justify-between
+  px-4 md:px-10
+  py-3
+  w-full
+  border-b border-gray-300
+">
+  {/* Logo */}
+  <div className="text-2xl md:text-3xl font-bold mb-2 md:mb-0">
+    <span className="text-yellow-500">Mon</span>
+    <span className="text-red-500">ke</span>
+    <span className="text-black">y</span>
+  </div>
 
-        <div className='me-10 flex gap-2 items-center'>
-            <Link to={"/"} className=" text-xl font-semibold">Home</Link>
-            <Link to={"register"} className='text-xl  bg-white-300 w-30 p-2 text-center outline-2  shadow-2xl outline-blue-400 rounded-3xl border-cyan-100  focus:outline-cyan-100'>Register</Link>
-             
-        </div>
-     </nav>
+  {/* Search */}
+  <div className="w-full md:w-auto mb-2 md:mb-0">
+    <input
+      type="text"
+      placeholder="Search product"
+      className="
+        w-full md:w-80
+        border
+        p-2
+        rounded-full
+        focus:outline-none
+        focus:ring-2 focus:ring-orange-400
+      "
+    />
+  </div>
+
+  {/* Links */}
+  <div className="flex gap-3 items-center">
+    <Link to="/" className="text-lg font-semibold">
+      Home
+    </Link>
+
+    <Link
+      to="/register"
+      className="
+        text-lg
+        px-4 py-2
+        rounded-full
+        shadow-lg
+        border
+        hover:bg-orange-100
+        transition
+      "
+    >
+      Register
+    </Link>
+  </div>
+</nav>
+
      </>
   );
 };

75b3ee130a254049bb01bd53f0fd23fb79f66dd4
Shivanand33 <chaurasiyashivanand01@gmail.com>
2026-01-21 16:54:53 +0530

    maine thik delete kiya

--- Tech-Project/client/src/config/Api.jsx
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL:"http://localhost:4500",
-})
-export default axiosInstance;
--- Tech-Project/server/src/routes/authRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { UserRegister } from "../controllers/authControllers.js";
-
-const router = express.Router();
-
-router.post("/register", UserRegister);
-
-export default router;

da2e78fc37b3b7c3e166898d89e8c07ca90d2e94
Shivanand33 <chaurasiyashivanand01@gmail.com>
2026-03-21 22:18:54 +0530

    cheackout

--- Creavings/client/src/pages/CheckoutPage.jsx
+++ Creavings/client/src/pages/CheckoutPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+ import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
@@ -503,7 +503,7 @@ const CheckoutPage = () => {
                 </div>
               </div>
 
-              {/* Place Order Button */}
+              {/* Place Order 
               <button
                 onClick={handlePlaceOrder}
                 disabled={isProcessing}
